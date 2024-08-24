1a300dbeb3c270d1eb3958b6719b5bf7cf569329
Abhimanyu Urs <abhiurs03@gmail.com>
2024-08-24 22:33:21 +0530

    asd didnt sync

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return render_template('generic.html')
+#@app.route('/')
+#def hello():
+#    return render_template('generic.html')
     
 @app.route('/', methods=['GET', 'POST'])
 def returnClass():
     return '<p> Class </p>'
+
